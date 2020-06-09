11e3358e28fa4db1f1f080ca78f2a767509d9134
Penchal <vemireddy.penchalreddy>
2020-06-09 15:42:44 +0530

    Initial Commit

---
+++ APICalls/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  APICalls
+//
+//  Created by VijayNew on 09/06/20.
+//  Copyright © 2020 VijayNew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
